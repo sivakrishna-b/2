6e3c64c742c2166461fc8fcf903b270c2cde57fd
sivakrishna <42761102+sivakrishna-b@users.noreply.github.com>
2018-09-08 11:10:59 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,8 +1,4 @@
-def power(base,exp):
-    if(exp==1):
-        return(base)
-    if(exp!=1):
-        return(base*power(base,exp-1))
-base=int(raw_input())
-exp=int(raw_input())
-print power(base,exp)
+a =int(input())
+b =int(input())
+c =int(a**b)
+print(c)
